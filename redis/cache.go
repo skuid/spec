58dbf4cca68a5050ef60275dfd809ffc6d78e9c0
MRHwick <MatthewRHardwick@gmail.com>
2017-05-02 16:37:47 -0400

    Address PR comments, reduce complexity, life is good

--- redis/cache.go
+++ redis/cache.go
@@ -1,14 +1,14 @@
 package redis
 
 import (
-	"github.com/go-redis/cache"
+	"gopkg.in/go-redis/cache.v6"
 	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
 // RedisCache is a *cache.Codec that can be set to the same cache you've created
 var RedisCache *cache.Codec
 
-// NewStandardRedisCache generates a preconfigured redis cache according to our spec.
+// NewStandardRedisCache generates a preconfigured redis cache according to our spec, using msgpack for serialization format.
 func NewStandardRedisCache(redisHostEnvironmentVariableName string) (codec *cache.Codec, err error) {
 	client, err := NewStandardRedisClient(redisHostEnvironmentVariableName)
 	if err != nil {
@@ -16,12 +16,8 @@ func NewStandardRedisCache(redisHostEnvironmentVariableName string) (codec *cach
 	}
 	codec = &cache.Codec{
 		Redis: client,
-		Marshal: func(v interface{}) ([]byte, error) {
-			return msgpack.Marshal(v)
-		},
-		Unmarshal: func(b []byte, v interface{}) error {
-			return msgpack.Unmarshal(b, v)
-		},
+		Marshal: msgpack.Marshal
+		Unmarshal: msgpack.Unmarshal,
 	}
 	return
 }
--- redis/client.go
+++ redis/client.go
@@ -3,30 +3,18 @@ package redis
 import (
 	"os"
 
-	"fmt"
-
-	"github.com/go-redis/redis"
+	"gopkg.in/redis.v6"
 )
+
+func init() {
+	redisHost := os.Getenv("REDIS_HOST")
+}
 
 //RedisClient is a *redis.Client that can be set to the same client you've created
 var RedisClient *redis.Client
 
 // NewStandardRedisClient generates a preconfigured redis client according to our spec.
-func NewStandardRedisClient(redisHostEnvironmentVariableName string) (client *redis.Client, err error) {
-	if redisHostEnvironmentVariableName == "" {
-		redisHostEnvironmentVariableName = "REDIS_HOST"
-	}
-
-	redisHost, present := os.LookupEnv("REDIS_HOST")
-	if !present {
-		err = fmt.Errorf("environment variable %s must be set in order to retrieve the redis hostname", redisHostEnvironmentVariableName)
-		return
-	}
-
-	client = redis.NewClient(&redis.Options{
-		Addr:     redisHost + ":6379",
-		Password: "",
-		DB:       0,
-	})
-	return
+func NewStandardRedisClient(options *redis.Options) *redis.Client {
+	options.Addr = redisHost + ":6379"
+	return redis.NewClient(options)
 }
