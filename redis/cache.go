e4be911958fd753fbdf55b6767a70680daab6f21
MRHwick <MatthewRHardwick@gmail.com>
2017-05-02 16:39:58 -0400

    Fix mistake about handling nonexistant error return
    Accept *redis.Options argument.
    
    Remove unnecessary export vars
    
    Remove unnecessary export vars
    
    fix mistake

--- redis/cache.go
+++ redis/cache.go
@@ -2,22 +2,15 @@ package redis
 
 import (
 	"gopkg.in/go-redis/cache.v6"
+	"gopkg.in/redis.v6"
 	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
-// RedisCache is a *cache.Codec that can be set to the same cache you've created
-var RedisCache *cache.Codec
-
 // NewStandardRedisCache generates a preconfigured redis cache according to our spec, using msgpack for serialization format.
-func NewStandardRedisCache(redisHostEnvironmentVariableName string) (codec *cache.Codec, err error) {
-	client, err := NewStandardRedisClient(redisHostEnvironmentVariableName)
-	if err != nil {
-		return
-	}
-	codec = &cache.Codec{
-		Redis: client,
+func NewStandardRedisCache(options *redis.Options) codec *cache.Codec {
+	return &cache.Codec{
+		Redis: NewStandardRedisClient(options),
 		Marshal: msgpack.Marshal
 		Unmarshal: msgpack.Unmarshal,
 	}
-	return
 }
--- redis/client.go
+++ redis/client.go
@@ -10,9 +10,6 @@ func init() {
 	redisHost := os.Getenv("REDIS_HOST")
 }
 
-//RedisClient is a *redis.Client that can be set to the same client you've created
-var RedisClient *redis.Client
-
 // NewStandardRedisClient generates a preconfigured redis client according to our spec.
 func NewStandardRedisClient(options *redis.Options) *redis.Client {
 	options.Addr = redisHost + ":6379"
