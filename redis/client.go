62b8c9edee08a21b440fdc44060c0b3a57013201
MRHwick <MatthewRHardwick@gmail.com>
2017-05-02 16:49:26 -0400

    Merge files

--- redis/client.go
+++ redis/client.go
@@ -3,7 +3,9 @@ package redis
 import (
 	"os"
 
+	"gopkg.in/go-redis/cache.v6"
 	"gopkg.in/redis.v6"
+	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
 func init() {
@@ -14,4 +16,13 @@ func init() {
 func NewStandardRedisClient(options *redis.Options) *redis.Client {
 	options.Addr = redisHost + ":6379"
 	return redis.NewClient(options)
+}
+
+// NewStandardRedisCache generates a preconfigured redis cache according to our spec, using msgpack for serialization format.
+func NewStandardRedisCache(options *redis.Options) codec *cache.Codec {
+	return &cache.Codec{
+		Redis: NewStandardRedisClient(options),
+		Marshal: msgpack.Marshal
+		Unmarshal: msgpack.Unmarshal,
+	}
 }
