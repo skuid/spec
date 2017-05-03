d2c2a1ab42c354d69fe14f53c10faa153caf2e23
MRHwick <MatthewRHardwick@gmail.com>
2017-05-03 08:32:34 -0400

    Fix some little errors

--- redis/client.go
+++ redis/client.go
@@ -3,13 +3,15 @@ package redis
 import (
 	"os"
 
-	"gopkg.in/go-redis/cache.v6"
-	"gopkg.in/redis.v6"
+	"github.com/go-redis/cache"
+	"github.com/go-redis/redis"
 	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
+var redisHost string
+
 func init() {
-	redisHost := os.Getenv("REDIS_HOST")
+	redisHost = os.Getenv("REDIS_HOST")
 }
 
 // NewStandardRedisClient generates a preconfigured redis client according to our spec.
@@ -21,10 +23,14 @@ func NewStandardRedisClient(options *redis.Options) *redis.Client {
 
 // NewStandardRedisCache generates a preconfigured redis cache according to our spec, using msgpack for serialization format.
 // Accepts an *redis.Options object, and overrides the Addr field to use `$REDIS_HOST:6379` instead
-func NewStandardRedisCache(options *redis.Options) codec *cache.Codec {
+func NewStandardRedisCache(options *redis.Options) *cache.Codec {
 	return &cache.Codec{
 		Redis: NewStandardRedisClient(options),
-		Marshal: msgpack.Marshal
-		Unmarshal: msgpack.Unmarshal,
+		Marshal: func(v interface{}) ([]byte, error) {
+			return msgpack.Marshal(v)
+		},
+		Unmarshal: func(b []byte, v interface{}) error {
+			return msgpack.Unmarshal(b, v)
+		},
 	}
 }
